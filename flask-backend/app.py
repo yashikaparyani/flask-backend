36304c1c568d5c4ddd63d12af5ba5074eb61742e
yashika paryani <yashikaparyani29@gmail.com>
2025-04-24 23:45:38 +0530

    fixed

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -137,7 +137,7 @@ def get_users():
     users = cursor.fetchall()
     conn.close()
     return jsonify([
-        {"id": row[0],"name":row[1],"email":row[2],"phone":row[3]}
+        {"id": row[0],"name":row[1],"email":row[2],"phone":row[4]}
         for row in users
     ])
 @app.route('/all-leaderboard', methods=['GET'])
