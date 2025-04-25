987dcf1c595ca8a9490d72eed8521596d878c371
yashika paryani <yashikaparyani29@gmail.com>
2025-04-25 16:58:50 +0530

    change

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -25,7 +25,7 @@ def init_db():
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL,
-            email TEXT NOT NULL,
+            email TEXT NOT NULL UNIQUE,
             password TEXT NOT NULL,
             phone TEXT NOT NULL
         )
